ce336e4f478451081f760dbdb301291e6d135df5
UmuhireAnuarithe <anuarithemuhire@gmail.com>
2019-09-05 11:24:46 +0200

    created class Accounts with a constructor  and docstring

---
+++ accounts.py
@@ -0,0 +1,11 @@
+class  Accounts :
+    '''
+    This a class that generates new instances of accounts .
+    '''
+
+    def __init__( self,names,usernname,password,email,location) :
+        
+        '''
+        __init__  methods that  help us to create  properties of our obbjects 
+
+        '''
