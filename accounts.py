556baeb474d63b77f50cafd97eaae6d0dd9832a8
UmuhireAnuarithe <anuarithemuhire@gmail.com>
2019-09-05 11:48:37 +0200

    added class variabble to hold object created  in a  class

--- accounts.py
+++ accounts.py
@@ -2,10 +2,25 @@ class  Accounts :
     '''
     This a class that generates new instances of accounts .
     '''
-
-    def __init__( self,names,usernname,password,email,location) :
+    Accounts_list = []
+    def __init__( self,names,usernname,password,phone_number,email) :
         
         '''
         __init__  methods that  help us to create  properties of our obbjects 
+         
 
+        Args:
+            names: New account  fullname.
+            username : New account username
+            passwod : New accounnt  password
+            p_number: New account phone number.
+            email : New  email address.
+            
         '''
+
+
+        self.names = names
+        self.username = usernname
+        self.password = password
+        self.phone_number  = phone_number
+        self.email =  email
